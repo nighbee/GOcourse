40ee8dfce43b06af92473065d4a6396f9d1c9580
Your Name <nightblue200416@gmail.com>
2024-09-13 12:08:09 +0500

    update namingConvention

--- Assigment1/WorkingWithJSONusingStructs.go
+++ Assigment1/WorkingWithJSONusingStructs.go
@@ -5,29 +5,29 @@ import (
 	"fmt"
 )
 
-type product struct {
+type Product struct {
 	Name     string  `json:"name"`
 	Price    float64 `json:"price"`
 	Quantity int     `json:"quantity"`
 }
 
-func toJSON(p product) string {
+func toJSON(p Product) string {
 	b, err := json.Marshal(p)
 	if err != nil {
 		return ""
 	}
 	return string(b)
 }
-func fromJSON(s string) product {
-	p := product{}
+func fromJSON(s string) Product {
+	p := Product{}
 	err := json.Unmarshal([]byte(s), &p)
 	if err != nil {
-		return product{}
+		return Product{}
 	}
 	return p
 }
 func main() {
-	product := product{Name: "Laptop", Price: 999.99, Quantity: 10}
+	product := Product{Name: "Laptop", Price: 999.99, Quantity: 10}
 	jsonStr := toJSON(product)
 	fmt.Printf("JSON string: %s\n", jsonStr)
 	decodeProduct := fromJSON(jsonStr)
